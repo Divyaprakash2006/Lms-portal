d8c5a89ae0ce2fd6faea3da04f7b9d365589b034
Divyaprakash <divyaprakash2006@example.com>
2026-01-11 14:14:17 +0530

    tt

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
    AXIOS INSTANCE
 ====================== */
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://lms-portal-u9ze.vercel.app/api',
 });
 
 /* ======================
