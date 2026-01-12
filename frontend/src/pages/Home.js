b128b70c88405cd5c79fe66a8aba2fd8752c47e5
Divyaprakash <divyaprakash2006@example.com>
2026-01-12 17:45:32 +0530

    Implement complete SEO fixes: Helmet, Meta tags, Canonical

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css';
-<link
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-  rel="stylesheet"
-/>
-
 import { Helmet } from 'react-helmet-async';
+import './Home.css';
 
 const Home = () => {
   return (
